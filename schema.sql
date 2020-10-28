ab3fea7383577f0cce1a74720242ea3d81b801de
Ekanpat <ekanpat83@gmail.com>
2020-10-28 12:48:14 -0500

    title and dept manager tables

--- schema.sql
+++ schema.sql
@@ -35,4 +35,23 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_emp (
+	dept_no VARCHAR NOT NULL,
+  emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE title (
+  emp_no INT NOT NULL,
+  title INT NOT NULL,
+	from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+  UNIQUE (title)
+);
+
 SELECT * FROM departments;

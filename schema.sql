931f1b0e77a42713e48cfcd49c58288d103d4fb7
Ekanpat <ekanpat83@gmail.com>
2020-10-28 20:28:15 -0500

    SQL Employees DB

--- schema.sql
+++ schema.sql
@@ -36,22 +36,18 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
-  emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-PRIMARY KEY (dept_no)
+	  emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+	  from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
 
-CREATE TABLE title (
-  emp_no INT NOT NULL,
-  title INT NOT NULL,
-	from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-  UNIQUE (title)
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+	  from_date DATE NOT NULL,
+	  to_date DATE NOT NULL,
+	  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	  PRIMARY KEY (title, emp_no, from_date)
 );
-
-SELECT * FROM departments;
